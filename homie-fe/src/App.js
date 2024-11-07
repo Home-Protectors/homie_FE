0908977dab04a8798c0d3eee3802662209ebc31e
Irene Choi <irenelove112@naver.com>
2024-11-08 08:26:31 +0900

    update : App.js

--- homie-fe/src/App.js
+++ homie-fe/src/App.js
@@ -3,19 +3,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header/Header';
 import IntroPage from './Dic/components/IntroPage';
+import CheckListPage from './CheckList/components/CheckListPage';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<IntroPage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+      return (
+            <Router>
+                  <div className="App">
+                        <Header />
+                        <Routes>
+                              <Route path="/" element={<IntroPage />} />
+                              <Route path="/checklist" element={<CheckListPage />} />
+                              <Route path="/tips" element={<div>Tips Page Coming Soon</div>} />
+                        </Routes>
+                  </div>
+            </Router>
+      );
 }
 
 export default App;
