baa375b14420b08ee1ef385619f677a3ed9e4f89
Irene Choi <irenelove112@naver.com>
2024-12-02 23:12:44 +0900

    fix : App.js

--- homie-fe/src/App.js
+++ homie-fe/src/App.js
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import { AuthProvider, useAuth } from './context/AuthContext'; 
+import { AuthProvider, useAuth } from './context/AuthContext';
 import Header from './Header/Header';
 import IntroPage from './Dic/components/IntroPage';
 import CheckListPage from './CheckList/components/CheckListPage';
@@ -10,116 +9,79 @@ import Item from './Item/components/Item';
 import DicPageCost from './Dic/components/DicPageCost';
 import MyPage from './MyPage/MyPage';
 import Login from './Login/Login';
-import SignUp from './Login/SignUp'; 
+import SignUp from './Login/SignUp';
 import './App.css';
 
-// ItemWrapper 컴포넌트 추가
 const ItemWrapper = ({ category }) => {
   return <Item initialCategory={category} />;
 };
 
-
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = useAuth(); 
+  const { isAuthenticated } = useAuth();
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
-
 function AppContent() {
   const location = useLocation();
-  const { isAuthenticated } = useAuth(); 
+  const { isAuthenticated } = useAuth();
 
   return (
     <div className="App">
-      {/* Render Header only if the user is authenticated */}
       {isAuthenticated && <Header />}
       <TransitionGroup>
         <CSSTransition key={location.key} timeout={400} classNames="page">
           <div className="page-container">
             <Routes location={location}>
-
-              <Route path="/" element={<IntroPage />} />
-              <Route path="/checklist" element={<CheckListPage />} />
-              
-              {/* /tips 경로에 Item 컴포넌트 연결 */}
-              <Route path="/tips" element={<ItemWrapper category="전체" />} />
-              <Route path="/tips/living" element={<ItemWrapper category="생활용품" />} />
-              <Route path="/tips/kitchen" element={<ItemWrapper category="주방용품" />} />
-              <Route path="/tips/cleaning" element={<ItemWrapper category="청소용품" />} />
-              <Route path="/tips/necessities" element={<ItemWrapper category="생필품" />} />
-
-              {/* Dic 페이지 연결*/}
-              <Route path="/Dic" element={<DicPageCost />} />
-                
               {/* Public Routes */}
               <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} /> {/* Add SignUp Route */}
-
+              <Route path="/signup" element={<SignUp />} />
+              
               {/* Protected Routes */}
-              <Route
-                path="/"
-                element={
-                  <ProtectedRoute>
-                    <IntroPage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/checklist"
-                element={
-                  <ProtectedRoute>
-                    <CheckListPage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/mypage"
-                element={
-                  <ProtectedRoute>
-                    <MyPage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/tips"
-                element={
-                  <ProtectedRoute>
-                    <ItemWrapper category="전체" />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/tips/living"
-                element={
-                  <ProtectedRoute>
-                    <ItemWrapper category="생활용품" />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/tips/kitchen"
-                element={
-                  <ProtectedRoute>
-                    <ItemWrapper category="주방용품" />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/tips/cleaning"
-                element={
-                  <ProtectedRoute>
-                    <ItemWrapper category="청소용품" />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/tips/necessities"
-                element={
-                  <ProtectedRoute>
-                    <ItemWrapper category="생필품" />
-                  </ProtectedRoute>
-                }
-              />
+              <Route path="/" element={
+                <ProtectedRoute>
+                  <IntroPage />
+                </ProtectedRoute>
+              } />
+              <Route path="/checklist" element={
+                <ProtectedRoute>
+                  <CheckListPage />
+                </ProtectedRoute>
+              } />
+              <Route path="/mypage" element={
+                <ProtectedRoute>
+                  <MyPage />
+                </ProtectedRoute>
+              } />
+              <Route path="/Dic" element={
+                <ProtectedRoute>
+                  <DicPageCost />
+                </ProtectedRoute>
+              } />
+              <Route path="/tips" element={
+                <ProtectedRoute>
+                  <ItemWrapper category="전체" />
+                </ProtectedRoute>
+              } />
+              <Route path="/tips/living" element={
+                <ProtectedRoute>
+                  <ItemWrapper category="생활용품" />
+                </ProtectedRoute>
+              } />
+              <Route path="/tips/kitchen" element={
+                <ProtectedRoute>
+                  <ItemWrapper category="주방용품" />
+                </ProtectedRoute>
+              } />
+              <Route path="/tips/cleaning" element={
+                <ProtectedRoute>
+                  <ItemWrapper category="청소용품" />
+                </ProtectedRoute>
+              } />
+              <Route path="/tips/necessities" element={
+                <ProtectedRoute>
+                  <ItemWrapper category="생필품" />
+                </ProtectedRoute>
+              } />
             </Routes>
           </div>
         </CSSTransition>
@@ -128,7 +90,6 @@ function AppContent() {
   );
 }
 
-// Main App Component
 function App() {
   return (
     <AuthProvider>
@@ -140,4 +101,3 @@ function App() {
 }
 
 export default App;
-
